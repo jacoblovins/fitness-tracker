b36698e88ad74b806f71e37e4f1d28647248e8e1
jacoblovins <lovins.jacob@yahoo.com>
2020-10-06 13:11:42 -0400

    added express routes

---
+++ routes/routes.js
@@ -0,0 +1,44 @@
+const router = require("express").Router();
+const Workout = require("../models/Workout");
+
+router.post("/api/workouts", ({ body }, res) => {
+    Workout.create(body)
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+router.put("/api/workouts/:id", ({ body }, res) => {
+    Workout.findByIdAndUpdate(req.params.id,{$push:{exercises: body}})
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+router.get("/api/workouts", (req, res) => {
+    Workout.find({})
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+router.get("/api/workouts/range", (req, res) => {
+    Workout.find({}).limit(7)
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+module.exports = router;
